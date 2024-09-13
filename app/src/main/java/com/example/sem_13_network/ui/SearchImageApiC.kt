de487ae8aafaea828484090c7726ff81958872f8
eastdrop <lyokha7777@gmail.com>
2024-09-13 15:13:34 +0300

    Fix SearchImageApiC.kt using moshi adapter

--- app/src/main/java/com/example/sem_13_network/ui/SearchImageApiC.kt
+++ app/src/main/java/com/example/sem_13_network/ui/SearchImageApiC.kt
@@ -1,5 +1,7 @@
 package com.example.sem_13_network.ui
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -8,9 +10,12 @@ import retrofit2.http.GET
 private const val BASE_URL = "https://api.thecatapi.com/"
 
 object RetrofitServices {
+    val moshi = Moshi.Builder()
+        .addLast(KotlinJsonAdapterFactory())
+        .build()
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(MoshiConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
 
     val searchImageApiC: SearchImageApiC = retrofit.create(SearchImageApiC::class.java)
